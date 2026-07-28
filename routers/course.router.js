e5119b7be310648f9b6ad3163bca90372701f80f
Ayush Kumar <helloayush135@gmail.com>
2026-07-28 14:09:28 +0530

    Update

--- routers/course.router.js
+++ routers/course.router.js
@@ -8,14 +8,15 @@ import {
     findCoursesByTrainerId,
     deleteCourse
 } from "../controllers/course.controller.js";
+import trainerAuthMiddleware from "../middlewares/trainerauth.middleware.js";
 
 const router = Router();
-router.route("/createCourse").post(createCourse);
+router.route("/createCourse").post(trainerAuthMiddleware, createCourse);
 router.route("/findCourses").get(findCourses);
 router.route("/findCoursesByTitle").get(findCoursesByTitle);
 router.route("/findCoursesByCategory").get(findCoursesByCategory);
 router.route("/findCoursesByLanguage").get(findCoursesByLanguage);
 router.route("/findCoursesByTrainerId").get(findCoursesByTrainerId);
-router.route("/deleteCourse").delete(deleteCourse);
+router.route("/deleteCourse").delete(trainerAuthMiddleware, deleteCourse);
 
 export { router as courseRouter }
