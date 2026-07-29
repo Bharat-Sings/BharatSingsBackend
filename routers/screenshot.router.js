e36c0be3bd92d62d9ccd4a79cb21f682555628e7
Ayush Kumar <helloayush135@gmail.com>
2026-07-29 11:53:53 +0530

    Update

--- routers/screenshot.router.js
+++ routers/screenshot.router.js
@@ -1,7 +1,7 @@
 import { Router } from "express";
 import { 
     createScreenshot 
-} from "../controllers/screenshot.controller";
+} from "../controllers/screenshot.controller.js";
 
 const router = Router();
 router.route("/createScreenshot").post(createScreenshot);
