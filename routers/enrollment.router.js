cee18436130f7b7ccd376d000329e3d6b2bfab05
Ayush Kumar <helloayush135@gmail.com>
2026-07-30 15:46:54 +0530

    Update

--- routers/enrollment.router.js
+++ routers/enrollment.router.js
@@ -5,11 +5,12 @@ import {
     findEnrollmentsByUserId 
 } from "../controllers/enrollment.controller.js";
 import authMiddleware from "../middlewares/auth.middleware.js";
+import trainerAuthMiddleware from "../middlewares/trainerauth.middleware.js";
 
 const router = Router();
 
 router.route("/createEnrollment").post(authMiddleware, createEnrollment);
-router.route("/findEnrollmentsByCourseId").get(findEnrollmentsByCourseId);
+router.route("/findEnrollmentsByCourseId").get(trainerAuthMiddleware, findEnrollmentsByCourseId);
 router.route("/findEnrollmentsByUserId").get(authMiddleware, findEnrollmentsByUserId);
 
 export { router as enrollmentRouter }
